2a90fc0556b4bec742bbfa55631ef53ea5efee57
Andrew Wilson <andrew.wilson90@hotmail.com>
2019-11-18 13:36:51 -0600

    all routes have some text

---
+++ src/components/Login/Login.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Login = () => {
+  return (
+    <div>
+      <h1>INSIDE LOGIN!</h1>
+    </div>
+  );
+};
+
+export default Login;
---
+++ src/components/Register/Register.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Register = () => {
+  return (
+    <div>
+      <h1>INSIDE REGISTER!</h1>
+    </div>
+  );
+};
+
+export default Register;
