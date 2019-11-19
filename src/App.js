6c94a400666d690746ae2baae869d27d75ad2a0c
Hector Valdez <hvaldez180@gmail.com>
2019-11-18 19:29:40 -0500

    saving initial commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
+import Home from "./components/Home/Home";
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
-      
+      <Home/>
     </div>
   );
 }
